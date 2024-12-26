03652c1eae2ad4b23fd88e32e5d59f70730ab1f5
Muhammad Lawal <abbaraees@gmail.com>
2024-12-26 01:58:19 +0100

    feat: Remove user registration functionality and related templates

--- app/auth.py
+++ app/auth.py
@@ -37,19 +37,6 @@ def logout():
     flash('You have been logged out.', 'green')
     return redirect(url_for('auth.login'))
 
-# Register Route
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Registration successful! You can now log in.', 'green')
-        return redirect(url_for('auth.login'))
-
-    return render_template('auth/register.html', form=form)
 
 # Unauthorized Handler
 @auth.app_errorhandler(401)
